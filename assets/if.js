d9c60941e43581f63bf9eec9ecfcea01b9ca1712
J. Francisco Martín <jfm.lisaso@gmx.com>
2023-02-19 13:22:04 +0100

    feat WIP: scene one

--- assets/if.js
+++ assets/if.js
@@ -1,15 +1,31 @@
+const LEFT_SHIFT_KEY_CODE = 16;
+const DOWN_ARROW_KEY_CODE = 40;
+
 const ifjs = {
-    config: {
-        highlightKeyCodes: [16, 40], // LeftShift, DownArrow
-        highlightStyleId: "highlights"
-      },
-      keyDown: () => {
-        console.log('key down');
-      },
-      keyUp: () => {
-        console.log('key up');
-      },
-      clearHyperlinks: () => {
-        console.log('clear hyperlinks');
-      },
-}
+  config: {
+    highlightKeyCodes: [LEFT_SHIFT_KEY_CODE, DOWN_ARROW_KEY_CODE],
+    highlightStyleId: "highlights"
+  },
+  keyDown: (event) => {
+    if (event.isComposing || event.keyCode === 229 || event.repeat) {
+      return;
+    }
+    if (ifjs.config.highlightKeyCodes.includes(event.keyCode)) {
+      document.getElementById(ifjs.config.highlightStyleId).disabled = true;
+    }
+  },
+  keyUp: (event) => {
+    if (event.isComposing || event.keyCode === 229) {
+      return;
+    }
+    if (ifjs.config.highlightKeyCodes.includes(event.keyCode)) {
+      document.getElementById(ifjs.config.highlightStyleId).disabled = false;
+    }
+  },
+  clearHyperlinks: () => {
+    console.log('clear hyperlinks');
+  },
+};
+
+document.onkeydown = function (event) { ifjs.keyDown(event); };
+document.onkeyup = function (event) { ifjs.keyUp(event); };
--- main.js
+++ main.js
@@ -16,8 +16,8 @@ function createWindow () {
 
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1420,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
